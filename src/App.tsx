65e4a93fecde2a6e82f164cc6d6bc222b52cb42c
Ritesh <riteshaggarwal2001@gmail.com>
2022-03-17 12:24:39 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import AppContainer from "./AppContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <div className="flex w-max p-4 mx-auto bg-white shadow-lg rounded-xl">
+        <img
+          src={logo}
+          className="w-12"
+          style={{
+            animation: "spin 2s linear infinite",
+          }}
+          alt="logo"
+        />
+        <h1 className="text-center text-xl">Level 1 milestone</h1>
+      </div>
+    </AppContainer>
   );
 }
 
