02180d8f83c73799eceb5321211e9bab0b477b9d
Ritesh <riteshaggarwal2001@gmail.com>
2022-03-22 19:24:10 +0530

    add search option

--- src/Components/FormsList.tsx
+++ src/Components/FormsList.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FormData } from "../types/formTypes";
 import { defaultFormsData, formData } from "../constants";
-import { Link, navigate } from "raviger";
+import { Link, navigate, useQueryParams } from "raviger";
 
 interface Props {}
 
@@ -14,6 +14,9 @@ const initialState: () => FormData[] = () => {
 
 function FormsList(props: Props) {
   const [list, setList] = useState(initialState);
+  const [{ search }] = useQueryParams();
+  const [searchString, setSearchString] = useState("");
+
   const openForm = (id: number) => {
     navigate(`/form/${id}`);
   };
@@ -40,7 +43,21 @@ function FormsList(props: Props) {
 
   return (
     <div className="mt-2">
-      <div className="flex justify-end mb-2">
+      <div className="flex justify-between items-center mb-2">
+        <div className="">
+          <form action="/" method="GET">
+            <input
+              className="outline text-black outline-slate-200 focus:ring-2 rounded-md px-2 text-lg"
+              onChange={(e) => {
+                setSearchString(e.target.value);
+              }}
+              name="search"
+              value={searchString}
+              type="text"
+              placeholder="Search"
+            />
+          </form>
+        </div>
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white rounded-lg px-4 py-2"
           onClick={newForm}
@@ -48,43 +65,47 @@ function FormsList(props: Props) {
           + New Form
         </button>
       </div>
-      {list.map((form) => {
-        return (
-          <div
-            className="p-4 border-t-2 flex justify-between items-center"
-            key={form.id}
-          >
-            <div className="font-semibold text-lg">{form.title}</div>
-            <div className="text-gray-500">
-              {form.formFields.length} Questions
+      {list
+        .filter((form) =>
+          form.title.toLowerCase().includes(search?.toLowerCase() || "")
+        )
+        .map((form) => {
+          return (
+            <div
+              className="p-4 border-t-2 flex justify-between items-center"
+              key={form.id}
+            >
+              <div className="font-semibold text-lg">{form.title}</div>
+              <div className="text-gray-500">
+                {form.formFields.length} Questions
+              </div>
+              <div className="flex gap-2">
+                <Link
+                  href={`/preview/${form.id}`}
+                  className="bg-blue-500 hover:bg-blue-700 text-white rounded-lg px-4 py-2"
+                >
+                  Take quiz
+                </Link>
+                <button
+                  onClick={() => {
+                    openForm(form.id);
+                  }}
+                  className="bg-blue-500 hover:bg-blue-700 text-white rounded-lg px-4 py-2"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => {
+                    deleteForm(form.id);
+                  }}
+                  className="bg-red-500 hover:bg-red-700 text-white rounded-lg px-4 py-2"
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-            <div className="flex gap-2">
-              <Link
-                href={`/preview/${form.id}`}
-                className="bg-blue-500 hover:bg-blue-700 text-white rounded-lg px-4 py-2"
-              >
-                Take quiz
-              </Link>
-              <button
-                onClick={() => {
-                  openForm(form.id);
-                }}
-                className="bg-blue-500 hover:bg-blue-700 text-white rounded-lg px-4 py-2"
-              >
-                Edit
-              </button>
-              <button
-                onClick={() => {
-                  deleteForm(form.id);
-                }}
-                className="bg-red-500 hover:bg-red-700 text-white rounded-lg px-4 py-2"
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
