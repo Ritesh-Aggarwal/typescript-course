bebb9890dbac2703eccdafe308f95cd9f323057d
Ritesh <riteshaggarwal2001@gmail.com>
2022-04-12 18:31:52 +0530

    refactor code

--- src/Components/FormsList.tsx
+++ src/Components/FormsList.tsx
@@ -10,6 +10,7 @@ import Paginator from "./common/Paginator";
 import { getCurrentUser } from "../App";
 import { User } from "../types/userTypes";
 import Copy from "../Copy";
+import { ListForm } from "./ListForm";
 
 const limit: number = 2;
 const fetchForms = async (
@@ -110,45 +111,12 @@ function FormsList() {
           )
           .map((form) => {
             return (
-              <div
-                className="p-4 border-t-2 flex justify-between items-center"
-                key={form.id}
-              >
-                <div className="font-semibold text-lg">{form.title}</div>
-                <div className="text-gray-500">
-                  {/* {form.formFields.length} Questions */}
-                </div>
-                <div className="flex gap-2">
-                  <Link
-                    href={`/form/${form.id}/submissions`}
-                    className="bg-blue-500 hover:bg-blue-700 text-white rounded-lg px-4 py-2"
-                  >
-                    View Submissions
-                  </Link>
-                  <Link
-                    href={`/preview/${form.id}`}
-                    className="bg-blue-500 hover:bg-blue-700 text-white rounded-lg px-4 py-2"
-                  >
-                    Take quiz
-                  </Link>
-                  {currentUser.username !== "" && (
-                    <Link
-                      href={`/form/${form.id}`}
-                      className="bg-blue-500 hover:bg-blue-700 text-white rounded-lg px-4 py-2"
-                    >
-                      Edit
-                    </Link>
-                  )}
-                  <button
-                    onClick={() => {
-                      handleDelete(form.id);
-                    }}
-                    className="bg-red-500 hover:bg-red-700 text-white rounded-lg px-4 py-2"
-                  >
-                    Delete
-                  </button>
-                  <Copy formId={form.id} />
-                </div>
+              <div key={form.id}>
+                <ListForm
+                  form={form}
+                  handleDeleteCB={handleDelete}
+                  currentUser={currentUser}
+                />
               </div>
             );
           })}
