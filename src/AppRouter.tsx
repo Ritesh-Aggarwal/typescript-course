ee0bf600112641572e096af2bc6d3663d5d710bf
Ritesh <riteshaggarwal2001@gmail.com>
2022-04-13 22:50:23 +0530

    setup sentry and lazy loading

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,31 +2,33 @@ import { useRoutes } from "raviger";
 import React from "react";
 import AppContainer from "./Components/AppContainer";
 import Header from "./Components/Header";
-import FormsList from "./Components/FormsList";
+// import FormsList from "./Components/FormsList";
 import About from "./Components/About";
-import Login from "./Components/Login";
+// import Login from "./Components/Login";
 import { User } from "./types/userTypes";
-import EditForm from "./Components/FormComponent/EditForm";
+// import EditForm from "./Components/FormComponent/EditForm";
 import PreviewForm from "./Components/PreviewForm/PreviewForm";
 import Submission from "./Components/Submission";
 import NotFound from "./Components/NotFound";
+import LoadingList from "./Components/LoadingList";
 
-// const routes = {
-//   "/": () => <FormsList />,
-//   "/login": () => <Login />,
-//   "/about": () => <About />,
-//   "/form/:id": ({ id }: { id: string }) => <EditForm formId={id} />,
-//   "/form/:id/submissions": ({ id }: { id: string }) => (
-//     <Submission formId={id} />
-//   ),
-//   "/preview/:formId": ({ formId }: { formId: string }) => (
-//     <PreviewForm formId={formId} />
-//   ),
-// };
+const Home = React.lazy(() => import("./Components/FormsList"));
+const LoginPage = React.lazy(() => import("./Components/Login"));
+const EditPage = React.lazy(
+  () => import("./Components/FormComponent/EditForm")
+);
 
 const publicRoutes = {
-  "/": () => <FormsList />,
-  "/login": () => <Login />,
+  "/": () => (
+    <React.Suspense fallback={<LoadingList />}>
+      <Home />
+    </React.Suspense>
+  ),
+  "/login": () => (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <LoginPage />
+    </React.Suspense>
+  ),
   "/about": () => <About />,
   "/form/:id/submissions": ({ id }: { id: string }) => (
     <Submission formId={id} />
@@ -36,7 +38,11 @@ const publicRoutes = {
   ),
 };
 const privateRoutes = {
-  "/form/:id": ({ id }: { id: string }) => <EditForm formId={id} />,
+  "/form/:id": ({ id }: { id: string }) => (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <EditPage formId={id} />
+    </React.Suspense>
+  ),
 };
 
 function AppRouter(props: { currentUser: User }) {
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
+
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: "https://753200a0eedf4048b6a86f622e43bdaa@o1202989.ingest.sentry.io/6328590",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +23,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
